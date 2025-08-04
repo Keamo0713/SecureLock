536272c059c58ea3ee8af8d5e59d73f4b79d4a75
Keamo0713 <selekeamogetsoe@gmail.com>
2025-08-04 11:26:06 +0200

    Update DocmentRequest.cs

--- pleSecureDoc/Models/DocmentRequest.cs
+++ pleSecureDoc/Models/DocmentRequest.cs
@@ -1,12 +1,12 @@
-﻿using System;
+using System;
 
 namespace pleSecureDoc.Models
 {
     public class DocumentRequest
     {
-        public string RequestId { get; set; }
-        public string EmployeeId { get; set; }
-        public string DocumentName { get; set; }
+        public string RequestId { get; set; } = string.Empty;
+        public string EmployeeId { get; set; } = string.Empty;
+        public string DocumentName { get; set; } = string.Empty;
         public DateTime RequestTime { get; set; }
         public bool IsApproved { get; set; }
         public DateTime? ApprovalTime { get; set; }
